ff8cf0902c76c28344a7979244c2cc23efc47693
Torstein <141224356+Trind03@users.noreply.github.com>
2024-04-15 13:59:03 +0200

    Added logic to test arg

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <cstring>
 
 int main(int argc, char *argv[])
 {
+    if(strcmp(argv[1],"server") == 0 || strcmp(argv[1],"Server"))
+        std::cout << "server";
 
+    else if(strcmp(argv[1],"client") == 0 || strcmp(argv[1],"Client"))
+        std::cout << "client";
+
+    else
+        std::cout << "Invalid arg";
+    
+    return 0;
 }
