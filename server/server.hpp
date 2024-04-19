cc40959da8078741a04ef34fc2b3bd8f7aef3664
Torstein <141224356+Trind03@users.noreply.github.com>
2024-04-19 23:21:54 +0200

    Added connection handler and a while loop

--- server/server.hpp
+++ server/server.hpp
@@ -2,12 +2,20 @@
 #include <iostream>
 #include <asio/include/asio.hpp>
 #include "utils.hpp"
+#include <thread>
 constexpr int port = 5554;
 int server()
 {
     asio::io_context io_context;
     asio::ip::tcp::endpoint con_details(asio::ip::tcp::v4(),port);
+    asio::ip::tcp::acceptor acceptor(io_context,con_details);
 
-    asio::ip::tcp::acceptor(io_context,con_details);
+    while(true)
+    {
+        asio::ip::tcp::socket socket(io_context);
+        std::thread thread(, socket);
 
+
+        socket.close();
+    }
 }
