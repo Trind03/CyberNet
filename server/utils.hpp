dcd2bab9c388e137e59b8dc7b15cd3bf9c963101
Torstein <141224356+Trind03@users.noreply.github.com>
2024-04-25 13:19:33 +0200

    Update
    Added logic to fetch client ip

--- server/utils.hpp
+++ server/utils.hpp
@@ -1,9 +1,11 @@
 #pragma once
+#include <iostream>
 #include <asio/include/asio.hpp>
 
 static void Connection_Handler(asio::ip::tcp::acceptor &acceptor, asio::ip::tcp::socket &socket)
 {
-    
-    acceptor.accept();
+    acceptor.accept(socket);
+    asio::ip::tcp::endpoint conn = socket.remote_endpoint();
+    asio::ip::address client_addr = conn.address();
     socket.close();
 } 
