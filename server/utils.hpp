ecb585da4f2cc5442ebff310ccbc6712c37b27bb
Torstein <141224356+Trind03@users.noreply.github.com>
2024-05-03 15:05:49 +0200

    Change of while loop and implementation  of try catch

--- server/utils.hpp
+++ server/utils.hpp
@@ -3,11 +3,23 @@
 #include <asio/include/asio.hpp>
 #include <thread>
 
+static bool running = true;
+
 static void Connection_Handler(asio::ip::tcp::acceptor &acceptor, asio::ip::tcp::socket &socket)
 {
-    acceptor.accept(socket);
-    asio::ip::tcp::endpoint conn = socket.remote_endpoint();
-    std::cout << "Connection established -> " << conn.address() << ":" << conn.port() << std::endl;
-
-    //socket.close();
+    try
+    {
+        while(running)
+        {
+            acceptor.accept(socket);
+            asio::ip::tcp::endpoint conn = socket.remote_endpoint();
+            std::cout << "Connection established -> " << conn.address() << ":" << conn.port() << std::endl;
+            socket.close();
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
 } 
