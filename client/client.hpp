aa54ffc6a90426583dd2aef99f76b3dc827b2659
Torstein <141224356+Trind03@users.noreply.github.com>
2024-05-13 13:24:47 +0200

    Extracted local ipv4 address to an independent variable

--- client/client.hpp
+++ client/client.hpp
@@ -3,11 +3,12 @@
 
 int client()
 {
+    asio::ip::address_v4 IPV4 = asio::ip::address_v4();
     constexpr int target_port = 5554;    
 
     asio::error_code error;
     asio::io_context io_context;
-    asio::ip::tcp::endpoint endpoint(asio::ip::make_address(target_ip,error),target_port);
+    asio::ip::tcp::endpoint endpoint(IPV4,target_port);
     asio::ip::tcp::socket sock(io_context);
 
     sock.connect(endpoint,error);
