47dd9c29023149c2ad3560daaf716cd9038ae580
Torstein <141224356+Trind03@users.noreply.github.com>
2024-05-14 13:55:30 +0200

    Added iterator

--- client/title.hpp
+++ client/title.hpp
@@ -2,11 +2,19 @@
 #include <functional>
 #include <memory>
 #include <fstream>
-
+#include <string>
 
 std::function<void(const char*)>boot_message_client = [=](const char* filename)
 {
     std::unique_ptr<std::fstream>(stream) = std::make_unique<std::fstream>(filename);
     //stream->open(filename);
 
+    if(stream->is_open())
+    {
+        std::string line;
+        while(std::getline((*stream),line))
+        {
+
+        }
+    }
 };
