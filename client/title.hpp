4a02065af59d3ec8cdea4cf15ebbcf13f1b4ed7e
Torstein <141224356+Trind03@users.noreply.github.com>
2024-05-14 13:49:48 +0200

    Added opem file instruction

--- client/title.hpp
+++ client/title.hpp
@@ -6,7 +6,8 @@
 
 std::function<void(const char*)>boot_message_client = [=](const char* filename)
 {
-    std::unique_ptr<std::fstream>(stream) = std::make_unique<std::fstream>(filename);
+    std::unique_ptr<std::fstream>(stream) = std::make_unique<std::fstream>();
 
-    
+    stream->open(filename);
+
 };
