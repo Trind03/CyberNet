862dfc74c5bafa530769cddfe1b793b52cca476c
Torstein <141224356+Trind03@users.noreply.github.com>
2024-05-16 14:22:45 +0200

    Update
    Added local system address instead of dynamic ip fetching

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <thread>
-#include <asio/asio/include/asio.hpp>
+#include <asio/include/asio.hpp>
 #include "title.hpp"
 
 static bool running = true;
@@ -9,7 +9,7 @@ int main()
 {
     const char* filename = "title.dat";
     std::thread title_client = std::thread(boot_message_client,filename);
-    asio::ip::address_v4 IPV4 = asio::ip::address_v4();
+    asio::ip::address IPV4 = asio::ip::address::from_string("127.0.0.1");
     constexpr int target_port = 5554;    
 
     asio::error_code error;
