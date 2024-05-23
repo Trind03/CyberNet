c7eba83a3d738bb7da80f2626080a5e589625d51
Torstein <141224356+Trind03@users.noreply.github.com>
2024-05-23 13:40:28 +0200

    Update
    Added broadcast function that's responsible for heartbeat signatures to server.

--- client/heartbeat_tx.h
+++ client/heartbeat_tx.h
@@ -10,6 +10,7 @@ struct heartbeat_tx : public std::enable_shared_from_this<heartbeat_tx>
 
     asio::ip::tcp::socket& get_socket();
 
+    void broadcast();
     asio::ip::tcp::socket sock;
     asio::steady_timer ping_timer;
 };
