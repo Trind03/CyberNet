1369fc230c6b2c6de9d68c8187f18f4a11929d1b
Torstein <141224356+Trind03@users.noreply.github.com>
2024-05-27 13:18:38 +0200

    added using namespaces

--- server/main.cpp
+++ server/main.cpp
@@ -6,7 +6,10 @@
 #include "command.h"
 #include "user.h"
 #include "title.hpp"
+#include <winsock.h>
 
+using namespace std;
+using namespace asio::ip;
 
 constexpr short port = 5554;
 
@@ -14,27 +17,25 @@ constexpr const char* filename = "title.dat";
 
 int main()
 {
-    std::thread t_boot_message = std::thread(boot_message_server,filename);
-    const asio::ip::tcp IPV4 = asio::ip::tcp::v4();
+    thread t_boot_message = thread(boot_message_server,filename);
+    const tcp IPV4 = tcp::v4();
     std::vector<user>users{};
     t_boot_message.join();
 
 
     command command(&users);
-    std::thread t_command_validator = std::thread(&command::command_handler,&command);
+    std::thread t_command_validator = thread(&command::command_handler,&command);
 
     asio::io_context io_context;
 
-    asio::ip::tcp::endpoint endpoint(asio::ip::tcp::v4(),port);
-    asio::ip::tcp::acceptor acceptor(io_context,endpoint);
+    tcp::endpoint endpoint(tcp::v4(),port);
+    tcp::acceptor acceptor(io_context,endpoint);
+    tcp::socket socket(io_context);
 
-    while(true)
-    {
-        asio::ip::tcp::socket socket(io_context);
-        Connection_Handler(acceptor,socket);
-    }
+
+    Connection_Handler(acceptor,socket);
+    t_command_validator.join();
     
 
-    t_command_validator.join();
     return EXIT_SUCCESS;
 }
--- server/utils.hpp
+++ server/utils.hpp
@@ -8,11 +8,9 @@ static void Connection_Handler(asio::ip::tcp::acceptor &acceptor, asio::ip::tcp:
 {
     try
     {
-        acceptor.listen();
         acceptor.accept(socket);
         asio::ip::tcp::endpoint conn = socket.remote_endpoint();
         std::cout << "Connection established -> " << conn.address() << ":" << conn.port() << std::endl;
-        socket.close();
     }
     catch(const std::exception& e)
     {
