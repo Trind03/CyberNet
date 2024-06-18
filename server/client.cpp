eea684492ac26d139084e2596743a0d077f02909
Torstein <141224356+Trind03@users.noreply.github.com>
2024-06-18 10:18:17 +0200

    Update
    Added time set and time since last ping  calculation.

--- server/client.cpp
+++ server/client.cpp
@@ -1,2 +1,20 @@
+#include <asio.hpp>
+#include <chrono>
 #include "client.h"
 
+void client::reset()
+{
+    time_stamp = std::chrono::system_clock::now();
+}
+
+float client::calculate_time()
+{
+    std::chrono::system_clock::time_point now_time = std::chrono::system_clock::now();
+    std::chrono::duration<float>result = now_time - time_stamp;
+    return result.count();
+}
+
+client::client(asio::ip::tcp::endpoint endpoint): Endpoint(endpoint)
+{
+    reset();
+}
--- server/client.h
+++ server/client.h
@@ -7,7 +7,9 @@ class client
 public:
     client(asio::ip::tcp::endpoint endpoint);
     client(client& clients) = delete;
+    float calculate_time();
+    void reset();
 private:
-    std::chrono::steady_clock Stamp;
+    std::chrono::system_clock::time_point time_stamp;
     asio::ip::tcp::endpoint Endpoint;
 };
