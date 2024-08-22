68acedda0fbfb5378c901a6412b9efdffc6934f7
Torstein <141224356+Trind03@users.noreply.github.com>
2024-08-22 14:05:01 +0200

    Update
    Renamed variables to comply with naming conventions. Added last members to init list in class ctor.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -2,7 +2,7 @@
 #include "client.h"
 
 client::client(asio::ip::address&& binding_addr,uint8_t&& port) : _Port(std::move(port)), _Binding_addr(std::move(binding_addr)),
-_Io_context(), _Sock(asio::ip::tcp::socket(_Io_context))
+_Io_context(), _Sock(asio::ip::tcp::socket(_Io_context)), _Endpoint(_Binding_addr, _Port), _Running(true)
 {
 
 }
--- src/client/client.h
+++ src/client/client.h
@@ -7,12 +7,13 @@ public:
     client(client&) = delete;
     client() = delete;
     client(asio::ip::address&& binding_addr,uint8_t&& port);
-    
+
 private:
+    asio::error_code _Error;
     asio::io_context _Io_context;
     asio::ip::tcp::socket _Sock;
-    asio::error_code _Error;
+    asio::ip::tcp::endpoint _Endpoint;
     const uint8_t _Port;
     const asio::ip::address _Binding_addr;
-    bool running;
+    bool _Running;
 };
