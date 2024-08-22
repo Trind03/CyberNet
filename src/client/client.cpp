a6bec76c28054a63307813aaf96687f36ad9dedf
Torstein <141224356+Trind03@users.noreply.github.com>
2024-08-22 16:50:05 +0200

    Update and bugfix

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -3,10 +3,20 @@
 #include "title.h"
 #include "client.h"
 
+#define Checkpoint(message) std::cout << message << std::endl;
+
 client::client(const char* filename,asio::ip::address&& binding_addr,const unsigned int&& port) : _Port(std::move(port)), _Binding_addr(std::move(binding_addr)),
 _Io_context(), _Sock(asio::ip::tcp::socket(_Io_context)), _Endpoint(_Binding_addr, _Port), _Running(true)
 {
-    std::cout << this->_Port << std::endl;
-    std::thread title_client = std::thread(std::bind(boot_message_client,filename));
+    try
+    {
+        std::thread title_client = std::thread(std::bind(boot_message_client,filename));
+        title_client.detach();
+    }
+    catch(std::exception& E)
+    {
+        std::cout << E.what() << std::endl;
+    }
+
 }
 
--- src/client/client.h
+++ src/client/client.h
@@ -9,10 +9,10 @@ public:
     client(const char* filename,asio::ip::address&& binding_addr,const unsigned int&& port);
 
     asio::error_code _Error;
+    const unsigned int _Port;
     asio::io_context _Io_context;
     asio::ip::tcp::socket _Sock;
     asio::ip::tcp::endpoint _Endpoint;
-    const unsigned int _Port;
     const asio::ip::address _Binding_addr;
     bool _Running;
 };
