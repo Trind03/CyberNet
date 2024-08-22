48491441b6a46b316901f762adee1e8983409000
Torstein <141224356+Trind03@users.noreply.github.com>
2024-08-22 16:37:15 +0200

    Bugfix

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -6,6 +6,7 @@
 client::client(const char* filename,asio::ip::address&& binding_addr,const unsigned int&& port) : _Port(std::move(port)), _Binding_addr(std::move(binding_addr)),
 _Io_context(), _Sock(asio::ip::tcp::socket(_Io_context)), _Endpoint(_Binding_addr, _Port), _Running(true)
 {
+    std::cout << this->_Port << std::endl;
     std::thread title_client = std::thread(std::bind(boot_message_client,filename));
 }
 
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -4,11 +4,12 @@
 #include "client.h"
 #include <memory>
 
+#define Checkpoint(message) std::cout << message << std::endl;
 int main()
 {
     std::unique_ptr<client>(Client) = std::make_unique<client>("title.dat",asio::ip::address::from_string("127.0.0.1"), 5554);
 
-    Client->_Sock.connect(Client->_Endpoint);
+    Client->_Sock.connect(Client->_Endpoint,Client->_Error);
 
     if(!Client->_Error)
     {
