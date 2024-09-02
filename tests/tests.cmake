5b139a9ebf7511ed9a52e86bbb6d1f0baabaa9ce
Torstein <141224356+Trind03@users.noreply.github.com>
2024-09-02 21:50:38 +0200

    Refactoring
    Added client class for unit testing.

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -7,12 +7,25 @@ set(Client_tests "tclient")
 set(tests_dir ${CMAKE_SOURCE_DIR}/tests/)
 add_subdirectory(GoogleTest)
 
+include_directories(
+
+    # Library
+    ${CMAKE_SOURCE_DIR}/asio/asio/include
+    ${CMAKE_SOURCE_DIR}/asio/asio/include/asio/detail
+    ${CMAKE_SOURCE_DIR}/asio/asio/include/asio
+    ${Boost_INCLUDE_DIRS}
+    ${CMAKE_SOURCE_DIR}../src/client/client.cpp
+)
 
 add_executable(${Server_tests} ${tests_dir}/server_tests.cpp)
-add_executable(${Client_tests} ${tests_dir}/client_test.cpp)
 
-target_link_libraries(${Server_tests}  gtest gtest_main)
-target_link_libraries(${Client_tests}  gtest gtest_main) 
+add_executable(${Client_tests} ${tests_dir}/client_test.cpp
+    ${CMAKE_SOURCE_DIR}../src/client/client.cpp
+    ${CMAKE_SOURCE_DIR}../src/client/title.cpp
+    )
+
+target_link_libraries(${Server_tests}  gtest gtest_main ws2_32 mswsock)
+target_link_libraries(${Client_tests}  gtest gtest_main ws2_32 mswsock) 
 
 add_test(NAME Server_test COMMAND ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${Server_tests})
 add_test(NAME Client_test COMMAND ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${Client_tests})
