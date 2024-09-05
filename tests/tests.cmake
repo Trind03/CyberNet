e3465f98840a3cb0ac389137a055e84e175d6481
Torstein <torsrinde@gmail.com>
2024-09-05 13:43:34 +0200

    Bugfix
    Added additional platform support for MacoSX and Linux.

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -14,26 +14,37 @@ include_directories(
     ${CMAKE_SOURCE_DIR}/asio/asio/include/asio/detail
     ${CMAKE_SOURCE_DIR}/asio/asio/include/asio
     ${Boost_INCLUDE_DIRS}
-    ${CMAKE_SOURCE_DIR}../src/client/client.cpp
+    ${CMAKE_SOURCE_DIR}/src/client/client.cpp
 )
 
 add_executable(${Server_tests}
-    ${tests_dir}/server_tests.cpp
-    ${CMAKE_SOURCE_DIR}../src/server/server.cpp
-    ${CMAKE_SOURCE_DIR}../src/server/session.cpp
-    ${CMAKE_SOURCE_DIR}../src/server/title.cpp
-    ${CMAKE_SOURCE_DIR}../src/server/commands.cpp
+    ${CMAKE_SOURCE_DIR}/tests/server_tests.cpp
+    ${CMAKE_SOURCE_DIR}/src/server/server.cpp
+    ${CMAKE_SOURCE_DIR}/src/server/session.cpp
+    ${CMAKE_SOURCE_DIR}/src/server/title.cpp
+    ${CMAKE_SOURCE_DIR}/src/server/commands.cpp
 
 )
 
 add_executable(${Client_tests}
     ${tests_dir}/client_test.cpp
-    ${CMAKE_SOURCE_DIR}../src/client/client.cpp
-    ${CMAKE_SOURCE_DIR}../src/client/title.cpp
+    ${CMAKE_SOURCE_DIR}/src/client/client.cpp
+    ${CMAKE_SOURCE_DIR}/src/client/title.cpp
     )
 
-target_link_libraries(${Server_tests}  gtest gtest_main ws2_32 mswsock)
-target_link_libraries(${Client_tests}  gtest gtest_main ws2_32 mswsock) 
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin" OR ${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+    target_link_libraries(${Server_tests}  gtest gtest_main)
+    target_link_libraries(${Client_tests}  gtest gtest_main)
+
+elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+    target_link_libraries(${Server_tests}  gtest gtest_main ws2_32 mswsock)
+    target_link_libraries(${Client_tests}  gtest gtest_main ws2_32 mswsock)
+
+else()
+    message("Error. Failed to identify system.")
+    return(-1)
+endif()
+
 
 add_test(NAME Server_test COMMAND ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${Server_tests})
 add_test(NAME Client_test COMMAND ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${Client_tests})
