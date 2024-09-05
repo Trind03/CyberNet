e78e8c550210f202613c50d9ab90afb637735db2
Torstein <torsrinde@gmail.com>
2024-09-05 22:03:04 +0200

    Update, added new code for abstract class

--- src/internet_computer.cpp
+++ src/internet_computer.cpp
@@ -6,7 +6,7 @@ std::chrono::system_clock::time_point internet_computer::reset()
     return std::chrono::system_clock::now();
 }
 
-float internet_computer::calculate_time(std::chrono::system_clock *time_stamp)
+float internet_computer::calculate_time(std::chrono::system_clock::time_point *time_stamp)
 {
     std::chrono::system_clock::time_point now_time = std::chrono::system_clock::now();
     std::chrono::duration<float>result = now_time - *(time_stamp);
--- src/internet_computer.h
+++ src/internet_computer.h
@@ -4,6 +4,8 @@
 
 class internet_computer
 {
+protected:
+    void virtual render_base() = 0;
     std::chrono::system_clock::time_point reset();
-    float calculate_time(std::chrono::system_clock *time_stamp);
+    float calculate_time(std::chrono::system_clock::time_point *time_stamp);
 };
